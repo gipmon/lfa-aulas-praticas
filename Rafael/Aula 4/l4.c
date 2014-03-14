4dec1164c9a4dfb7cd7a7e17d504bdd75fe9b005
Rafael Ferreira <mail@rafaelferreira.pt>
2014-03-14 22:52:02 +0000

    aula 4 l1 e l4 - nao sei como fazer o l4 no init state

---
+++ Rafael/Aula 4/l4.c
@@ -0,0 +1,64 @@
+#include <stdbool.h>
+#include <string.h>
+
+/* number of symbols in input alphabet */
+#define NInputSymbols 3
+
+const char* inputAlphabet = "abc";
+
+/* a way to get the ord of a symbol */
+static int ordOf(char c)
+{
+    char* p = strchr((char*)inputAlphabet, c);
+    return (p==NULL) ? -1 : p - inputAlphabet;
+}
+
+/* number of states */
+#define NStates 4
+
+/* define state names */
+enum { a, b, c, ab};
+
+/* the initial state */
+#define initialState a
+
+/* the transition table */
+static const int delta[NStates][NInputSymbols] =
+{
+      /*         'e'    'd'  */
+    { /* DDD */  EDD,   EED  },
+    { /* EED */  DED,   EDE  },
+    { /* EDE */  DDE,   DEE  },
+    { /* DEE */  EEE,   DDD  },
+    { /* EDD */  DDD,   DED  },
+    { /* DED */  EED,   DDE  },
+    { /* DDE */  EDE,   EEE  },
+    { /* EEE */  DEE,   EDD  }
+};
+
+/* the transition table */
+static const char gamma[NStates][NInputSymbols] =
+{
+      /*         'e'    'd'  */
+    { /* DDD */  'E',   'E' },
+    { /* EED */  'C',   'C' },
+    { /* EDE */  'C',   'C' },
+    { /* DEE */  'E',   'D' },
+    { /* EDD */  'C',   'C' },
+    { /* DED */  'E',   'C' },
+    { /* DDE */  'E',   'E' },
+    { /* EEE */  'C',   'C' }
+};
+
+void mealy(const char* u, char* v)
+{
+    int i;
+    int state = initialState; // the current state, initialized */
+    for (i=0; i<strlen(u); i++)
+    {
+    v[i] = gamma[state][ordOf(u[i])];
+        state = delta[state][ordOf(u[i])];
+    }
+
+  v[i] = '\0';
+}
